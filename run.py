7506a38c5117328a70c751a7961e67787677a315
StefFrustagli <>
2023-11-07 17:39:17 +0000

    Docs: add comments with guidelines in run.py

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+1. User opens terminal. Welcome message appears with nursery rhyme to explain the game.
+2. User asked if they want to play. 
+  2B If 'yes', the game starts. 
+  2C If 'no', the game exits.
+3. If 2.B, the game starts displaying a random dashed version of the word -- 
+   dashes are displayed instead of letters (user knows the length of the word). 
+   When correct letter is guessed, it will replace the dash.
+  3A Info given to the player:
+      - Word: _ _ _ _ _
+      - Length: num
+      - Letters guessed: a, b 
+      - Attempts left: num
+4. User asked to choose a letter.
+5. 5A If letter is wrong, a message appears: "Letter wromg. Try again." Input to choose another letter.
+      A part of the hangman picture is shown. It is added at the bottom of the info provided above. 
+      This will happen for seven times, the attempts the player has. When the picture is complete, the player loses the game
+   5B If the letter is right, the letter will be displayed in the correct position. 
+      The user will be asked to guess again till the attempts end or the word is completed.
+
+
+"""
