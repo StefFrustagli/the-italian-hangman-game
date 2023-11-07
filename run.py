42cabf32febf5fcd960e7391ef1196b00b41e540
StefFrustagli <>
2023-11-07 17:54:53 +0000

    Chore: add Hangman Game visual rapresentation

--- run.py
+++ run.py
@@ -39,3 +39,59 @@ WORD_BANK_THREE = "cammello, rinoceronte, elefante, ermellino, bisonte, canguro,
 
 # Number of attempts
 NUM_ATTEMPTS = 7
+
+def little_man_pic():
+        print("  +---+")
+        print("  O   |")
+        print(" /|\\  |")
+        print(" / \\  |")
+        print("     ===")        
+
+
+def little_man_one():
+        print("       ")
+        print("       ")
+        print("       ")
+        print("       ")
+        print("     ===")  
+def little_man_two():
+        print("        ")
+        print("        ")
+        print("        ")
+        print("      |")
+        print("     ===")  
+
+def little_man_three():
+        print("        ")
+        print("        ")
+        print("      |")
+        print("      |")
+        print("     ===")  
+
+def little_man_four():
+        print("        ")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("     ===")  
+
+def little_man_five():
+        print("    --+")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("     ===")  
+
+def little_man_six():
+        print("  +---+")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("     ===")  
+
+def little_man_seven():
+        print("  +---+")
+        print("  O   |")
+        print(" /|\\  |")
+        print(" / \\  |")
+        print("     ===")  
